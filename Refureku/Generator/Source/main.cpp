97e92615b6746967b1ed74119604b44157196b2e
Julien SOYSOUVANH <julien.soysouvanh@gmail.com>
2021-09-16 17:16:43 +0200

    [Update] Kodgen to last version

--- Refureku/Generator/Source/main.cpp
+++ Refureku/Generator/Source/main.cpp
@@ -136,7 +136,7 @@ void parseAndGenerate(fs::path&& settingsFilePath)
 	loadSettings(logger, codeGenMgr.settings, fileParser.getSettings(), codeGenUnitSettings, std::forward<fs::path>(settingsFilePath));
 
 	//Parse
-	kodgen::CodeGenResult genResult = codeGenMgr.run(fileParser, codeGenUnit, false, 2u);
+	kodgen::CodeGenResult genResult = codeGenMgr.run(fileParser, codeGenUnit, false);
 
 	//Result
 	printGenerationResult(logger, genResult);
