cca947b3f1779a6156ca4ed4609b969736767bc6
Julien SOYSOUVANH <julien.soysouvanh@gmail.com>
2021-09-19 18:57:18 +0200

    [Add] Import/Export macros to prepare transition to dynamic library

--- Refureku/Library/Include/Refureku/Config.h
+++ Refureku/Library/Include/Refureku/Config.h
@@ -19,6 +19,45 @@
 
 #endif
 
-#define REFUREKU_VERSION_MAJOR 1
-#define REFUREKU_VERSION_MINOR 4
+#define REFUREKU_VERSION_MAJOR 2
+#define REFUREKU_VERSION_MINOR 0
 #define REFUREKU_VERSION_PATCH 0
+
+//Dynamic library import/export macros
+#if defined _WIN32 || defined __CYGWIN__
+
+	#ifdef REFUREKU_EXPORT
+
+		#ifdef __GNUC__
+			#define REFUREKU_API __attribute__((dllexport))
+		#else
+			#define REFUREKU_API __declspec(dllexport)
+		#endif
+
+	#else
+
+		#ifdef __GNUC__
+			#define REFUREKU_API	__attribute__((dllimport))
+		#else
+			#define REFUREKU_API	__declspec(dllimport)
+		#endif
+
+  #endif
+
+	#define REFUREKU_INTERNAL
+
+#else
+
+	#if __GNUC__ >= 4
+
+		#define REFUREKU_API		__attribute__((visibility("default")))
+		#define REFUREKU_INTERNAL	__attribute__((visibility("hidden")))
+
+	#else
+
+		#define REFUREKU_API
+		#define REFUREKU_INTERNAL
+
+	#endif
+
+#endif
