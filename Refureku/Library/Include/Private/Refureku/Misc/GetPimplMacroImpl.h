d6d1cffa8638eaebf994988c35a48e72f7d39eac
Julien SOYSOUVANH <julien.soysouvanh@gmail.com>
2021-10-13 19:49:15 +0200

    [Add] CI CodeCov generation flow + [Rename] CMake GEN_PROPERTY_RTTI > RFK_GEN_PROPERTY_RTTI

--- Refureku/Library/Include/Private/Refureku/Misc/GetPimplMacroImpl.h
+++ Refureku/Library/Include/Private/Refureku/Misc/GetPimplMacroImpl.h
@@ -12,3 +12,4 @@
 #define GEN_GET_PIMPL(type, source) \
     RFK_NODISCARD inline type* getPimpl() noexcept { return reinterpret_cast<type*>(source); } \
     RFK_NODISCARD inline type const* getPimpl() const noexcept { return reinterpret_cast<type const*>(source); }
+
