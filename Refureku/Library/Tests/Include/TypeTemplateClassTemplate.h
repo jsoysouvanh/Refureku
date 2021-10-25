e39b2b40fa752f59f08be5643c8846a96fb36809
Julien SOYSOUVANH <julien.soysouvanh@gmail.com>
2021-10-25 20:03:17 +0200

    [Update] Class template tests

--- Refureku/Library/Tests/Include/TypeTemplateClassTemplate.h
+++ Refureku/Library/Tests/Include/TypeTemplateClassTemplate.h
@@ -7,8 +7,6 @@
 
 #pragma once
 
-//#include <vector>
-
 #include <Refureku/Object.h>
 #include <Refureku/Properties/ParseAllNested.h>
 
@@ -72,42 +70,4 @@ namespace TESTNP NAMESPACE()
 //	TESTCLASS_GENERATED
 //};
 
-//template <typename T>
-//class CLASS() Vector : public std::vector<T>
-//{
-//	friend rfk::internal::CodeGenerationHelpers;
-//	friend implements_template1__rfk_registerChildClass<Vector<T>, void, void(rfk::Struct&)>;
-//
-//	private: template <typename ChildClass> static void _rfk_registerChildClass(rfk::Struct & childClass) noexcept
-//	{
-//		rfk::internal::CodeGenerationHelpers::registerChildClass<std::vector<T>, ChildClass>(childClass);
-//		rfk::Struct const& thisClass = staticGetArchetype();
-//		if constexpr (!std::is_same_v<ChildClass, Vector<T>>) const_cast<rfk::Struct&>(thisClass).addSubclass(childClass);
-//	}
-//
-//
-//	public: static rfk::ClassTemplateInstantiation const& staticGetArchetype() noexcept {
-//		static bool initialized = false;
-//		static rfk::ClassTemplateInstantiation type("Vector", std::hash<std::string>()(std::string("c:@ST>1#T@Vector") + rfk::internal::getTypename<Vector>()), sizeof(Vector), 1, *rfk::getArchetype<::Vector>());
-//		if (!initialized)
-//		{
-//			initialized = true;
-//			static rfk::TypeTemplateArgument arg0(rfk::getArchetype<T>());
-//			type.addTemplateArgument(arg0);
-//			type.setDirectParentsCapacity(1);
-//			type.addDirectParent(rfk::getArchetype<std::vector>(), static_cast<rfk::EAccessSpecifier>(1));
-//			Vector<T>::_rfk_registerChildClass<Vector<T>>(type);
-//			static rfk::StaticMethod defaultSharedInstantiator("", 0u, rfk::getType<rfk::SharedPtr<Vector<T>>>(), new rfk::NonMemberFunction<rfk::SharedPtr<Vector<T>>()>(&rfk::internal::CodeGenerationHelpers::defaultSharedInstantiator<Vector<T>>), rfk::EMethodFlags::Default, nullptr);
-//			type.addSharedInstantiator(defaultSharedInstantiator);
-//		}
-//		return type;
-//	}
-//
-//	private: static inline rfk::ClassTemplateInstantiationRegisterer _rfk_registerer = staticGetArchetype(); rfk::internal::ForceGenerateSymbol<&_rfk_registerer> _rfk_forceRegister() = delete;
-//
-//	//Vector_T_GENERATED
-//};
-
 File_TypeTemplateClassTemplate_GENERATED
-
-//template class CLASS() Vector<int>;
